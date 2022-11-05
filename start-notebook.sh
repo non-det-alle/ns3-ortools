70acded85f2633d85e8cedae249d50ac01118246
Alessandro Aimi <alleaimi95@gmail.com>
2022-11-05 20:25:12 +0100

    Remove Hub suppport

--- start-notebook.sh
+++ start-notebook.sh
@@ -8,11 +8,6 @@ set -e
 # JupyterLab by default
 DOCKER_STACKS_JUPYTER_CMD="${DOCKER_STACKS_JUPYTER_CMD:=lab}"
 
-if [[ -n "${JUPYTERHUB_API_TOKEN}" ]]; then
-    echo "WARNING: using start-singleuser.sh instead of start-notebook.sh to start a server associated with JupyterHub."
-    exec /usr/local/bin/start-singleuser.sh "$@"
-fi
-
 wrapper=""
 if [[ "${RESTARTABLE}" == "yes" ]]; then
     wrapper="run-one-constantly"
